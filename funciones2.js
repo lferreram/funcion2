8d84b696bace51be35807c7f5f49b5dd5cb3226d
lferreram <lferreram@yahoo.com>
2023-05-15 09:57:45 -0600

    first commit

---
+++ funciones2.js
@@ -0,0 +1,13 @@
+
+let persona={nombre:"Larissa",mensaje:"Bienvenida"}
+
+saludar(persona)
+function saludar(Bienvenida)
+{console.log(`Hola ${Bienvenida.nombre} ${Bienvenida.mensaje}`) }
+
+////    
+let personas = {nombre:"Larissa",apellido:"Ferrera"}
+
+saludos (personas)
+function saludos(inicial)
+{console.log (`Hola ${inicial.nombre} ${inicial.apellido}`)}
